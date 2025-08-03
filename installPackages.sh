0a60862249086a07788d5f564e95cf0cc95674f7
Valerie <valeria.levenets94@gmail.com>
2025-08-03 12:16:55 +0300

    -

---
+++ installPackages.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pacman -Suy \
+curl cpupower docker-compose easyeffects \
+ffmpeg ffmpeg4.4 firefox flac git \
+gimp kdenlive htop kdeconnect libldac \
+lm_sensors lsp-plugins lsb-release \
+make media-player-info mesa mesa-utils \
+nano networkmanager-openvpn okular \
+openh264 openjpeg2 openssh openssl \
+openvpn power-profiles-daemon powertop \
+solaar spectacle steam tailscale \
+telegram-desktop vlc vlc-plugins-all \
+wget x264 x265 xcalib
