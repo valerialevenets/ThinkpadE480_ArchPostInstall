5fce672c1fd19a0cb6e2b82c33e607abfb6f878a
Valerie <valeria.levenets94@gmail.com>
2025-08-04 23:54:36 +0300

    -

--- setupIntelUndervolt.sh
+++ setupIntelUndervolt.sh
@@ -4,6 +4,8 @@ sed -i 's/undervolt 0 \x27CPU\x27 0/undervolt 0 \x27CPU\x27 -100/g' /etc/intel-u
 sed -i 's/undervolt 1 \x27GPU\x27 0/undervolt 1 \x27GPU\x27 -50/g' /etc/intel-undervolt.conf
 sed -i 's/undervolt 2 \x27CPU Cache\x27 0/undervolt 2 \x27CPU Cache\x27 -100/g' /etc/intel-undervolt.conf
 
+sed -i 's/enable no/enable yes/g' /etc/intel-undervolt.conf
+
 intel-undervolt apply
 systemctl enable intel-undervolt.service
 systemctl start intel-undervolt.service
